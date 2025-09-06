a0d565574bba02a13960973b17e6b1e8dc6e4033
Islambek Gazizov <islambekgazizovv@gmail.com>
2025-09-06 15:26:51 +0500

    Added roles

---
+++ src/Roles.cs
@@ -0,0 +1,10 @@
+﻿namespace Talapker.Identity.Contracts;
+
+public static class Roles
+{
+    public const string Admin = "Admin";
+    public const string Zavuch = "Zavuch";
+    public const string Teacher = "Teacher";
+    
+    public static List<string> ListOfRoles = [Roles.Admin, Roles.Zavuch, Roles.Teacher];
+}
